5ee83a08226a6ef679c0e56ba84e1fceeb499cac
Vlad8947 <31621413+Vlad8947@users.noreply.github.com>
2019-03-18 01:58:38 +0300

    recConMod-set file length

--- pc_ui/src/main/java/com/local_movement/pc_ui/model/ReceiveConnectionModel.java
+++ pc_ui/src/main/java/com/local_movement/pc_ui/model/ReceiveConnectionModel.java
@@ -1,5 +1,6 @@
 package com.local_movement.pc_ui.model;
 
+import com.local_movement.core.Converter;
 import com.local_movement.core.model.FileProperties;
 import com.local_movement.core.model.MovementProperties;
 import javafx.beans.property.SimpleStringProperty;
@@ -14,7 +15,7 @@ public class ReceiveConnectionModel {
     private StringProperty userName = new SimpleStringProperty();
     private StringProperty address = new SimpleStringProperty();
     private StringProperty fileName = new SimpleStringProperty();
-    private StringProperty size = new SimpleStringProperty();
+    private StringProperty fileLength = new SimpleStringProperty();
 
     private MovementProperties movementProperties;
 
@@ -24,5 +25,7 @@ public class ReceiveConnectionModel {
         userName.setValue(fileProperties.getUserName());
         address.setValue(movementProperties.getAddress());
         fileName.setValue(fileProperties.getFileName());
+        fileLength.setValue(Converter.length(
+                movementProperties.getFileProperties().getFileLength()));
     }
 }
