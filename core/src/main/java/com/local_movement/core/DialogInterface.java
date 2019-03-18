7ad7851ec4db2c40e61714f686d12e6b5c7a1716
Vlad8947 <31621413+Vlad8947@users.noreply.github.com>
2019-03-18 22:21:34 +0300

    Update DialogInterface.java

--- core/src/main/java/com/local_movement/core/DialogInterface.java
+++ core/src/main/java/com/local_movement/core/DialogInterface.java
@@ -4,6 +4,6 @@ public interface DialogInterface {
 
     void error(String title, String header, String content);
 
-    String textInput(String defaultText, String title, String header, String content);
+    String textInput(String defaultText, String title, String header, String content) throws InterruptedException;
 
 }
